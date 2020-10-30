4a9bd65c96ed2a16ae6321db843e5c0ac1d5fa12
PiotrSakowski <polokolsek@gmail.com>
2020-10-30 11:52:01 +0100

    poruszanie2

---
+++ Assets/Poruszanie.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poruszanie : MonoBehaviour
+{
+    public float thrustW = 1.0f;
+    public Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * thrustW);
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * (-thrustW));
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.right * (-thrustW));
+
+        }
+   
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * thrustW);
+
+        }
+    }
+}
