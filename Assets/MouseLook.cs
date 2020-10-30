2f30425b56ca16860ab50e6e858dfd15d68f2d57
PiotrSakowski <polokolsek@gmail.com>
2020-10-30 12:02:52 +0100

    poruszanie3

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -1,19 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// MouseLook rotates the transform based on the mouse delta.
-// To make an FPS style character:
-// - Create a capsule.
-// - Add the MouseLook script to the capsule.
-//   -> Set the mouse look to use MouseX. (You want to only turn character but not tilt it)
-// - Add FPSInput script to the capsule
-//   -> A CharacterController component will be automatically added.
-//
-// - Create a camera. Make the camera a child of the capsule. Position in the head and reset the rotation.
-// - Add a MouseLook script to the camera.
-//   -> Set the mouse look to use MouseY. (You want the camera to tilt up and down like a head. The character already turns.)
 
-[AddComponentMenu("Control Script/Mouse Look")]
 public class MouseLook : MonoBehaviour
 {
     public enum RotationAxes
@@ -34,7 +22,6 @@ public class MouseLook : MonoBehaviour
 
     void Start()
     {
-        // Make the rigid body not change rotation
         Rigidbody body = GetComponent<Rigidbody>();
         if (body != null)
             body.freezeRotation = true;
