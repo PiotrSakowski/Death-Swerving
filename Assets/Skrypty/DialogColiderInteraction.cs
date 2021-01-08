f4fe7853de1da2caa0b79009641e849a5fa6c939
PiotrSakowski <polokolsek@gmail.com>
2021-01-08 21:02:51 +0100

    drzwiNaprawione

---
+++ Assets/Skrypty/DialogColiderInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogColiderInteraction : MonoBehaviour
+{
+    public GameObject ColiderUruchamiajacy;
+    DialogNPC dialog;
+    public GameObject PanelDialogowy;
+
+    void Awake()
+    {
+        dialog = GetComponent<DialogNPC>();
+        PanelDialogowy = GameObject.Find("Panel_Dialogowy1");
+    }
+    
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if ((collision.gameObject == ColiderUruchamiajacy))
+        {
+            WykonywaczDialogu.dialog = dialog.DialogPostaci;
+            PanelDialogowy.SetActive(true);
+
+        }
+
+    }
+
+        }
