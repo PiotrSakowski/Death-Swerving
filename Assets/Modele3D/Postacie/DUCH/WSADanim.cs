ba3971c7bd47da79150f0be5c1165ebb2a76f4ac
PiotrSakowski <polokolsek@gmail.com>
2021-01-08 04:11:55 +0100

    duchMesh

---
+++ Assets/Modele3D/Postacie/DUCH/WSADanim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WSADanim : MonoBehaviour
+{
+    Animator m_Animator;
+    
+    void Start()
+    {
+
+        m_Animator = gameObject.GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            m_Animator.SetBool("Wpress", true);
+        else
+            m_Animator.SetBool("Wpress", false);
+
+        if (Input.GetKeyDown(KeyCode.S))
+            m_Animator.SetBool("Spress", true);
+        else
+            m_Animator.SetBool("Spress", false);
+
+        if (Input.GetKeyDown(KeyCode.A))
+            m_Animator.SetBool("Apress", true);
+        else
+            m_Animator.SetBool("Apress", false);
+
+        if (Input.GetKeyDown(KeyCode.D))
+            m_Animator.SetBool("Dpress", true);
+        else
+            m_Animator.SetBool("Dpress", false);
+
+    }
+}
---
+++ Assets/Modele3D/Postacie/DUCH/WSADlerp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WSADlerp : MonoBehaviour
+{
+    public Transform pion;
+    public Transform w, s, a, d;
+    public float speed = 0.01f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.rotation = Quaternion.Lerp(pion.rotation, w.rotation, Time.time * speed);
+        else
+            transform.rotation = Quaternion.Lerp(w.rotation, pion.rotation, Time.time * speed);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.rotation = Quaternion.Lerp(pion.rotation, s.rotation, Time.time * speed);
+        else
+            transform.rotation = Quaternion.Lerp(s.rotation, pion.rotation, Time.time * speed);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.rotation = Quaternion.Lerp(pion.rotation, a.rotation, Time.time * speed);
+        else
+            transform.rotation = Quaternion.Lerp(a.rotation, pion.rotation, Time.time * speed);
+
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.rotation = Quaternion.Lerp(pion.rotation, d.rotation, Time.time * speed);
+        else
+            transform.rotation = Quaternion.Lerp(d.rotation, pion.rotation, Time.time * speed);
+
+    }
+}
