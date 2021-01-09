ce2c9981f1a05343ad3f46e9da6ec5b87e7d00d2
PiotrSakowski <polokolsek@gmail.com>
2021-01-09 17:40:03 +0100

    RandomPozycja

---
+++ Assets/Skrypty/RandomRozmieszczacz.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRozmieszczacz : MonoBehaviour
+{
+    public GameObject pozycja1, pozycja2, pozycja3, pozycja4;
+    int[] ToLoad = new int[] { 0, 1, 2, 3, 4 };
+    
+    
+    void Start()
+    {
+        int RandomIndex = (int)System.Math.Min(ToLoad.Length - 1, Random.Range(0, ToLoad.Length));
+
+        if(RandomIndex == 1)
+            transform.position = pozycja1.transform.position;
+
+        if (RandomIndex == 2)
+            transform.position = pozycja2.transform.position;
+
+        if (RandomIndex == 3)
+            transform.position = pozycja3.transform.position;
+
+        if (RandomIndex == 4)
+            transform.position = pozycja4.transform.position;
+
+        if (RandomIndex == 0)
+            transform.position = pozycja1.transform.position;
+
+        Debug.Log("wylosowano" + RandomIndex);
+
+    }
+    
+}
